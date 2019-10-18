2378a32b4fcd93a7a24b2b7ca64238ee4f53250b
muyun123 <1539579866@qq.com>
2019-10-18 20:32:55 +0800

    dai05

--- 三阶段/day03/js/http.js
+++ 三阶段/day03/js/http.js
@@ -1,20 +1,30 @@
-let http = require('http');
-const querystring = require('querystring');
-http.createServer(function (request, response) {
-    response.setHeader('Access-Control-Allow-Origin', '*')
-    response.writeHead(200)
-    var d = '';
-    request.on('data', function (chunk) {
-        d += chunk;
-    })
-    request.on('end', function () {
-        var {
-            password,
-            username
-        } = querystring.parse(d);
-        console.log(password, username);
-    })
-    response.write('hello world123')
-    response.end()
-}).listen(8001);
-console.log('qi');
+// let http = require('http');
+// const querystring = require('querystring');
+// http.createServer(function (request, response) {
+//     response.setHeader('Access-Control-Allow-Origin', '*')
+//     response.writeHead(200)
+//     var d = '';
+//     request.on('data', function (chunk) {
+//         d += chunk;
+//     })
+//     request.on('end', function () {
+//         var {
+//             password,
+//             username
+//         } = querystring.parse(d);
+//         console.log(password, username);
+//     })
+//     response.write('hello world123')
+//     response.end()
+// }).listen(8001);
+// console.log('qi');
+var name = "The Window";
+var object = {
+    name: "My Object",
+    getNameFunc: function () {
+        return function () {
+            return this.name;
+        }
+    }
+};
+console.log(object.getNameFunc()());
